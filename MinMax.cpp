4e621daafe9ec9a05fed5abd17d60e16376afd1b
saklayen-dev <saklayen@media365.com.bd>
2025-08-20 09:48:35 +0600

    Added new code

---
+++ MinMax.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+
+
+    int a,b;
+    cin >> a >> b;
+
+    if (a < b)
+    {
+        cout << a;
+    }
+    else {
+        cout << b;
+    }
+
+
+
+}
