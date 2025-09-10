5057790486298e4036cd3576915a3bfa6e4a311f
gm-sourav <souravmahbub13@gmail.com>
2025-09-10 12:55:14 +0600

    Second

---
+++ String_element_access.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    cout << s[3] << endl;
+    cout << s[s.size() - 1] << endl;
+    cout << s.at(1) << endl;
+    cout << s.back() << endl;
+    cout << s.front() << endl;
+
+
+
+    
+    return 0;
+}
---
+++ string_modifier.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "Gm";
+    string s1 = "Sourov";
+    s += s1;
+    cout << s;
+
+
+    
+    return 0;
+}
