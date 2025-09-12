b8d6019dce1175fab0aaa1dbc830263d93b46d9a
gm-sourav <souravmahbub13@gmail.com>
2025-09-12 11:28:55 +0600

    Second

---
+++ String_input_with_space.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    //cin >> s;
+    getline(cin,s);
+    cin.ignore();
+    cout << s << endl;
+
+
+    
+    return 0;
+}
