78ecbe7170d29493ae85ca8a2cbe783b91eb3e25
gm-sourav <souravmahbub13@gmail.com>
2025-09-12 11:17:23 +0600

    Second

---
+++ String_itearator.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "sourov";
+
+    // cout << *s.begin() << endl;
+    // cout << *(s.end() - 1) << endl;
+
+    for (auto  ite = s.begin(); ite < (s.end()); ite++)
+    {
+       cout << *ite << endl;
+    }
+    
+
+
+    
+    return 0;
+}
