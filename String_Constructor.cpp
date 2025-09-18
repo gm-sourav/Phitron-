76ac41f979a08170ba36b282304db63bff21083f
gm-sourav <souravmahbub13@gmail.com>
2025-09-18 12:59:02 +0600

    Second

---
+++ String_Constructor.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // string s = "sourov";
+    // string s("sourov");
+    // cout << s;
+    string s = "Gm Sourov";
+    string t(s, 4);
+    cout << t << endl;
+
+    return 0;
+}
